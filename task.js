a0dea04b4109c6c449623752e0f8f6ecc15c713c
AleksandrEpishkin <aepishkin@yandex.ru>
2022-09-14 19:30:07 +0300

    ïnitial commit

---
+++ task.js
@@ -0,0 +1,15 @@
+function compareArrays(arr1, arr2) {
+  let result;
+
+  result = (arr1.length === arr2.length) && (arr1.every((n, i) => n === arr2[i]));
+
+  return result; // boolean
+}
+
+function advancedFilter(arr) {
+  let resultArr;
+
+  resultArr = arr.filter(e => e > 0).filter(e => e % 3 ===0).map(item => (item * 10));
+
+  return resultArr; // array
+}
